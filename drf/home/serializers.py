cec1b16b850be84083fbce90a3c925a2389e4872
Amir <amir@debian-BULLSEYE-live-builder-AMD64>
2021-10-27 09:18:41 -0400

    add serializer field to serializer.py and rewrite it by ModelSerializer

--- drf/home/serializers.py
+++ drf/home/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 
+from .models import Person
 
-class PersonSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-    age = serializers.IntegerField()
-    email = serializers.EmailField()
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ('id', 'name', 'age', 'email')
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
