6b9a390ad6d71a6013a1455d5bdb07129416bdef
= <amirmahdi7776@gmail.com>
2021-10-28 14:20:26 +0330

    modify the views that using modelSerializer and comment something

--- drf/home/views.py
+++ drf/home/views.py
@@ -2,22 +2,23 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import PersonSerializer
 from .models import Person
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
 def test(request):
     if request.method == 'POST':
         name = request.data['name']
-        return Response({'name': f'{name}'})
+        return Response({'name': f'{name}'}, status=status.HTTP_200_OK)
     else:
-        return Response({'name': 'Amirmahdi'})
+        return Response({'name': 'Amirmahdi'}, status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
 def show_persons(request):
     persons = Person.objects.all()
     ser_data = PersonSerializer(persons, many=True)
-    return Response(ser_data.data)
+    return Response(ser_data.data, status=status.HTTP_200_OK)
 
 
 @api_view()
@@ -27,18 +28,19 @@ def show_person(request, id):
 
     except Person.DoesNotExist:
 
-        return Response({'Error': "the person doesNotExist"})
+        return Response({'Error': "the person doesNotExist"}, status=status.HTTP_404_NOT_FOUND)
 
     ser_data = PersonSerializer(person)
-    return Response(ser_data.data)
+    return Response(ser_data.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def create_person(request):
     info = PersonSerializer(data=request.data)
     if info.is_valid():
-        Person(name=info.validated_data['name'], age=info.validated_data['age'],
-               email=info.validated_data['email']).save()
-        return Response({'message': 'ok'})
+        # Person(name=info.validated_data['name'], age=info.validated_data['age'],      ##doesnt need in modelSerializer
+        #        email=info.validated_data['email']).save()
+        info.save()
+        return Response({'message': 'ok'}, status=status.HTTP_201_CREATED)
     else:
-        return Response(info.errors)
+        return Response(info.errors, status=status.HTTP_400_BAD_REQUEST)
